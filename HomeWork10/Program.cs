e67cbfac01e827ac23a842c2e29d89a0d8cb5679
belan <smakki.92@gmail.com>
2024-12-13 17:18:31 +0300

    Добавьте файлы проекта.

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,58 @@
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace HomeWork10
+{
+    internal class Program
+    {
+        public static string token { get; } = "<YOUR TOKEN HERE>";
+        static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var botClient = new TelegramBotClient(token);
+            var commands = new[] {
+                new BotCommand { Command = "/cat",Description = "Случайный факт о кошках"},
+            };
+            await botClient.SetMyCommands(commands, cancellationToken: cts.Token);
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+            var handler = new UpdateHandler();
+            handler.OnHandleUpdateStarted += (Message message) => Console.WriteLine($"Началась обработка сообщения {message.Text}");
+            handler.OnHandleUpdateCompleted += (Message message) => Console.WriteLine($"Закончилась обработка сообщения {message.Text}");
+            
+            try
+            {
+               
+                
+                botClient.StartReceiving(handler, receiverOptions, cancellationToken: cts.Token);
+
+                var me = await botClient.GetMe(cancellationToken: cts.Token);
+                Console.WriteLine($"{me.FirstName} запущен!");
+                Console.WriteLine("Нажмите клавишу A для выхода");
+                while (!cts.IsCancellationRequested)
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.A)
+                    {
+                        cts.Cancel();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{me.FirstName} продолжает работать!");
+                    }
+                    await Task.Delay(1, cancellationToken: cts.Token);
+                }
+            }
+            finally
+            {
+                cts.Cancel();
+                handler.OnHandleUpdateStarted -= (Message message) => Console.WriteLine($"Началась обработка сообщения {message.Text}");
+                handler.OnHandleUpdateCompleted -= (Message message) => Console.WriteLine($"Закончилась обработка сообщения {message.Text}");
+            }
+        }
+    }
+}
---
+++ HomeWork10/UpdateHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace HomeWork10
+{
+    internal class UpdateHandler : IUpdateHandler
+    {
+        public delegate void MessageHandler(Message message);
+        public event MessageHandler OnHandleUpdateStarted;
+        public event MessageHandler OnHandleUpdateCompleted;
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(exception);
+            return Task.CompletedTask;
+        }
+        record CatFactDto(string Fact, int length);
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            OnHandleUpdateStarted?.Invoke(update.Message);
+            if (update.Type == UpdateType.Message)
+            {
+                if(update.Message.Text == "/cat")
+                {
+                    using var client = new HttpClient();
+                    var catFact = await client.GetFromJsonAsync<CatFactDto>("https://catfact.ninja/fact", cancellationToken);
+                    botClient.SendMessage(update.Message.Chat.Id, catFact.Fact);
+                }
+                await botClient.SendMessage(update.Message.Chat.Id, "Сообщение успешно принято");
+            }
+            OnHandleUpdateCompleted?.Invoke(update.Message);
+        }
+    }
+}
